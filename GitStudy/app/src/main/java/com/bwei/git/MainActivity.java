da0a8cdbef0842e77a7fafcacb6910131cee1b5b
forwardforever <jnbfeng@163.com>
2018-04-10 09:58:00 +0800

    first commit code

---
+++ GitStudy/app/src/main/java/com/bwei/git/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bwei.git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //这是我使用git版本控制工具提交代码
+    }
+}
